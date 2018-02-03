75b6a1af0d63f448ff273dbfd5ec75ff088e9cbe
Jing <qinj1994@163.com>
2018-02-03 08:46:42 +0100

    Update Basics.py

--- beginnner/Basics.py
+++ beginnner/Basics.py
@@ -25,7 +25,7 @@ atlanta_float = 1379.5
 ## 5. The Type Function ##
 
 atlanta_string = 'Atlanta_string'
-print(type(atlanta_string))
+print(type(atlanta_string)) #show the type of variables
 
 ## 6. Using A List To Store Multiple Values ##
 
@@ -45,16 +45,23 @@ crime_rates.append(1379)
 
 print(cities)
 print(crime_rates)
-
+# in one list, data type can be different,but using more space
+# array.array can only contains one data type
+# when it comes to MATH, import numpy
+# refer from: https://stackoverflow.com/questions/176011/python-list-vs-array-when-to-use
 
 ## 7. Creating Lists With Values ##
 
 crime_rates = [749,371,828,503,1379]
+# or use list.append(thing_you_wang_to_add)
+# or create a list A = [], innitial_value = 0, A = [innitial_value for a in range(10)]
 
 ## 8. Comments ##
 
 crime_rates = [749, 371, 828, 503, 1379]#ciuciuciuciuciuci
 print(crime_rates)#hahahahahah
+
+# to make life easier and make your collegue happy~
 
 ## 9. Accessing Elements In A List ##
 
@@ -63,6 +70,12 @@ crime_rates = [749, 371, 828, 503, 1379]
 
 anchorage_str = cities[2]
 anchorage_cr = crime_rates[2]
+
+# we can use the index with positive or negative integer
+# A[0] is the first element of the list
+# A[-1] is the last element of the list
+# A[0] = A[-len(A)]
+# A[-1] = A[len(A) - 1]
 
 ## 10. Retrieving The Length Of A List ##
 
